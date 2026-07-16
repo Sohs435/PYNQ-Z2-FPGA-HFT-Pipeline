870cf01c6f760a91db3a51b1819c4d079737e969
Sohs435 <141024024+Sohs435@users.noreply.github.com>
2026-07-17 01:08:35 +0530

    Update receive_udp.py

--- pynq/receive_udp.py
+++ pynq/receive_udp.py
@@ -1,9 +1,9 @@
 import socket
 
-LISTEN_IP = "0.0.0.0"
+LISTEN_IP = "0.0.0.0" #Check all possible IPs 
 LISTEN_PORT = 5001
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPV4 + UDP AF_INET6 for IPV6 
 sock.bind((LISTEN_IP, LISTEN_PORT))
 
 print(f"Listening on UDP port {LISTEN_PORT}...")
